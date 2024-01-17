94aa0bce1b793a574de34bfc8824c9e17c8790a4
spyro5555 <eugeniusz.buczynski@gmail.com>
2024-01-17 18:28:40 +0100

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// zadanie domowe dzień 4
+
+string name = "Ewa";
+var sex = "kobieta";
+int age = 30;
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if ((!(sex == "kobieta")) && age < 18)
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("nie jestem w stanie sklasyfikować");
+}
